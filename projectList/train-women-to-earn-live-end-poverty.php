5ad6f19f9025db00335cccc09b4f0e8d18c489c1
vihaan56 <vihaansingla555@gmail.com>
2020-07-14 21:23:43 +0530

    new files added

---
+++ projectList/train-women-to-earn-live-end-poverty.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <title>Document</title>
+    <style>
+        #currentHeading {
+            margin-top: 15px !important;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img class=" lazyloaded" src="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img/http://www.iwcwtministry.org/wp-content/uploads/2014/08/LOGOiwcwtministryORG.png" data-src="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img/http://www.iwcwtministry.org/wp-content/uploads/2014/08/LOGOiwcwtministryORG.png"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="projectDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Projects
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="projectDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="involveDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Get Involved
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="involveDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact us</a>
+                    </li>
+                </ul>
+                <button type="button" class="btn btn-warning"><i>Donate Now</i></button>
+
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container" style="margin-top:40px;">
+        <div class="row">
+
+            <div class="col-md-8 col-sm-12 content-area sidebar_right">
+
+                <div class="inner">
+                    <article class="projects-detail">
+                        <header class="entry-header">
+                            <h2 class="entry-title">CHILD PROTECTION</h2>
+                        </header>
+                        <div class="entry-content" style="margin-top:30px;">
+                            <p style="text-align: justify;"><noscript><img class="wp-image-2111 aligncenter" src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_582,h_377/http://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education.jpg" alt="" width="582" height="377" srcset="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_1980/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education.jpg 1980w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_300/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-300x194.jpg 300w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_768/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-768x496.jpg 768w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_1024/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-1024x662.jpg 1024w" sizes="(max-width: 582px) 100vw, 582px" /></noscript><img class="wp-image-2111 aligncenter lazyloaded" src="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_582,h_377/http://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education.jpg" data-src="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_582,h_377/http://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education.jpg" alt="" width="582" height="377" data-srcset="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1980/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education.jpg 1980w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_300/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-300x194.jpg 300w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_768/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-768x496.jpg 768w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_1024/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-1024x662.jpg 1024w" data-sizes="(max-width: 582px) 100vw, 582px" sizes="(max-width: 582px) 100vw, 582px" srcset="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1980/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education.jpg 1980w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_300/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-300x194.jpg 300w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_768/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-768x496.jpg 768w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_1024/https://www.iwcwtministry.org/wp-content/uploads/2016/04/Support-Education-1024x662.jpg 1024w"></p>
+                            <p style="text-align: justify;">&nbsp;</p>
+                            <p style="text-align: justify;">The Child Protection Programme is a core sector of our work. We utilize a child rights programming framework and keep in mind the cross-cutting themes of child participation, non-discrimination and best interests of children.</p>
+                            <p style="text-align: justify;"><strong>Our child protection work focuses on three key “evidence” groups:</strong></p>
+                            <p>Children affected by disasters/emergencies and conflict, including Disaster Risk Reduction.</p>
+                            <p>Exploitation &amp; child trafficking.</p>
+                            <p>Children in the worst forms of labour and children with inadequate parental care including alternatives to institutional care.</p>
+                            <p style="text-align: justify;">Understanding good and best practices in care and protection is a major focus of our programme work. This requires quality monitoring and evaluation (research &amp; studies) in order to identify good practices in documentation and sharing of this evidence.</p>
+                            <p style="text-align: justify;"><strong>Some of the most important activities of our Child Protection programme include:</strong></p>
+                            <p>Improving the understanding of the situation of vulnerable children in need of care and protection.</p>
+                            <p>Building children’s resilience and supporting their participation in their own protection, including child-led organisations and child-to-child support.</p>
+                            <p>Promoting diversion from inappropriate or punitive responses and encouraging the reintegration of children who have been stigmatised because of their coping strategies in the absence of effective protection mechanisms.</p>
+                            <p>Demonstrating the benefits of preventative approaches and early intervention over interventions at a later stage.</p>
+                            <p>Supporting the development of community-based care and protection systems.</p>
+                            <p>Support to the co-ordination and integration of services and support to vulnerable children.</p>
+                            <p>Encouraging moves away from services directed towards particular problems towards services addressed to supporting the functioning and coping strategies of children and families.</p>
+                            <p>Building the care and protection of children into broader social welfare, poverty reduction and other national development strategies.</p>
+                            <p>Building the capacity of duty bearers to deliver effective care and protection.</p>
+                            <p>Advocating for legal and policy reform in line with the principles and standards of the Child Rights For Change and other relevant international and regional instruments.</p>
+                            <!-- <p><a href="http://www.iwcwtministry.org/make-a-donation/" hidefocus="true" style="outline: none;"><noscript><img class="alignnone size-full wp-image-1837" src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_301,h_57/http://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate.png" alt="" width="301" height="57" srcset="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_301/https://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate.png 301w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_300/https://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate-300x57.png 300w" sizes="(max-width: 301px) 100vw, 301px" /></noscript><img class="alignnone size-full wp-image-1837 lazyloaded" src="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_301,h_57/http://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate.png" data-src="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_301,h_57/http://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate.png" alt="" width="301" height="57" data-srcset="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_301/https://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate.png 301w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_300/https://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate-300x57.png 300w" data-sizes="(max-width: 301px) 100vw, 301px" sizes="(max-width: 301px) 100vw, 301px" srcset="https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_301/https://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate.png 301w, https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_300/https://www.iwcwtministry.org/wp-content/uploads/2016/10/Donate-300x57.png 300w"></a></p> -->
+                            <button type="button" class="btn btn-warning btn-lg"><i>Donate here</i></button>
+                            <!-- <button type="button" class="btn btn-warning btn-lg btn-block"><i>Donate Here</i></button> -->
+                           
+                            <!-- <div class="divider project-details"></div> -->
+                        </div>
+                    </article>
+                    <br>
+
+                    <!-- <section class="projects-details-navigation clearfix">
+                        <span class="prev_pag"><a href="https://www.iwcwtministry.org/all-project-list/self-help-group/" rel="prev" hidefocus="true" style="outline: none;"><i class="icon-chevron-left"></i><span>Previous Project</span></a></span>
+                        <span class="next_pag"><a href="https://www.iwcwtministry.org/all-project-list/train-women-to-earn-live-end-poverty/" rel="next" hidefocus="true" style="outline: none;"><span>Next Project</span><i class="icon-chevron-right"></i></a></span>
+                    </section> -->
+
+                </div>
+            </div>
+            <!-- <div class="col-md-4 col-sm-12 sidebar widget-area">
+                <div class="inner">
+                    <div class="widget widget-text ">
+                        <h3 class="widget-title">Facebook Updates</h3>
+                        <p><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIWCWT.Ministry%2F&amp;tabs=timeline&amp;width=340&amp;height=500&amp;small_header=false&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId=1376848959015285" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowtransparency="true"></iframe></p>
+
+                       <div class="social-links">
+                            <span class="social-icons">
+                                <a href="https://www.facebook.com/IWCWT.Ministry" title="Facebook" target="_blank" hidefocus="true" style="outline: none;"><i class="tficon-facebook"></i></a>
+                                <a href="https://www.twitter.com/IWCWT_NGO" title="Twitter" target="_blank" hidefocus="true" style="outline: none;"><i class="tficon-twitter"></i></a>
+                                <a href="https://www.instagram.com/iwcwt_ngo" title="Instagram" target="_blank" hidefocus="true" style="outline: none;"><i class="tficon-instagram"></i></a>
+                            </span>
+                        </div> 
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+</body>
+
+</html>
